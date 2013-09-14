caa6684ebd7ebfc420f621a627b582dd77f6fb19
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-09-14 10:05:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wattsun::Application.config.secret_key_base = '09b07ad9b1d1c168c515ad6978058db3755bf08f2ab47680e914360493837f086ba7e7d131727e9e97ea1f44d65b37ae65634ada58f65af2609a4de9a41ef8d1'
