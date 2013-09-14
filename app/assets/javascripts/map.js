d29fe7a81e4cd8e8e148ec07b1e635fd149fb9bd
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-09-14 10:48:38 -0700

    leaflet integrated

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  // create a map in the "map" div, set the view to a given place and zoom
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  // add an OpenStreetMap tile layer
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">  OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  // add a marker in the given location, attach some popup content to it   and open the popup
+  L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+      .openPopup();
+
+  L.tileLayer('http://{s}.tile.cloudmade.  com/26d0b59d4044441e8bb7ebecb61a875f/997/256/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">  OpenStreetMap</a> contributors, <a href="http://creativecommons.  org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://  cloudmade.com">CloudMade</a>',
+      maxZoom: 18
+  }).addTo(map);
+});
---
+++ app/controllers/wattsun_controller.rb
@@ -0,0 +1,10 @@
+class WattsunController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+end
