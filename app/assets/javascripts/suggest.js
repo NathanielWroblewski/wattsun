c06bdfe7a820105beea635141862fac9fa145b44
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-09-14 17:46:52 -0700

    fixed location suggest

--- app/assets/javascripts/suggest.js
+++ app/assets/javascripts/suggest.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   if ($('#address').length > 0) {
-    $("#address").suggest({ key: 'AIzaSyA5UiGrefmQJrDpCG7ZnIaVZP0ASpIf9FU', filter:'(all type:/location/citytown)'});
+    $("#address").suggest({ key: 'AIzaSyA5UiGrefmQJrDpCG7ZnIaVZP0ASpIf9FU', filter:'(all type:/location/location)'});
   }
 });
