98fdc412f37af0516cde37af7647505e35947f15
kenrett <kenrettberg@gmail.com>
2013-10-21 16:11:39 -0700

    again again again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,15 +17,13 @@ Wattsun::Application.configure do
   # For Heroku
   config.assets.initialize_on_precompile = false
 
-  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
