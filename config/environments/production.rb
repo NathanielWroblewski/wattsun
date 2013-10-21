64e2f5a520142aabf6a05caa2ed0a704b7245b8b
kenrett <kenrettberg@gmail.com>
2013-10-21 16:02:45 -0700

    added assets.precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Wattsun::Application.configure do
   # For Heroku
   config.assets.initialize_on_precompile = false
 
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
