e754e82db6d70a6652539fbc4ea52b1d9d5e38af
kenrett <kenrettberg@gmail.com>
2013-10-21 16:17:39 -0700

    removed commented out code

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,6 @@ Bundler.require(:default, Rails.env)
 module Wattsun
   class Application < Rails::Application
     
-    # For Heroku:
-    # config.assets.initialize_on_precompile = false
 
-    # config.serve_static_assets = true
-
-    # If you set config.assets.initialize_on_precompile to false, be sure to test rake assets:precompile locally before deploying. It may expose bugs where your assets reference application objects or methods, since those are still in scope in development mode regardless of the value of this flag. Changing this flag also affects engines. Engines can define assets for precompilation as well. Since the complete environment is not loaded, engines (or other gems) will not be loaded, which can cause missing assets.
   end
 end
